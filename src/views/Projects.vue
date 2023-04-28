86ad72a0bdb808e03c36105ef5fe6485ea7cc233
batuhanarslan <batuhan.ars@yahoo.com>
2023-04-28 11:45:44 +0300

    Proje form yapısı hazır.

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -11,7 +11,7 @@
                             <h3 class="panel-title">
                                 <input type="text" placeholder="Ara..." class="form-control">
                             </h3>
-                            <a href="#" class="btn btn-primary">
+                            <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#createProject">
                                 <i class="fas fa-plus"></i>
                                 Proje Ekle
                             </a>
@@ -63,9 +63,24 @@
                 </div>
             </footer>
         </div>
+        <Modal id="createProject" title="Proje Oluştur" aria-labelledby="createProjectLabel" label-id="createProjectLabel">
+            <form>
+                <div class="modal-body">
+                    <Input type="file" label="Logo" for-label="logo" />
+                    <Input type="text" label="Başlık" for-label="title" />
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Kapat</button>
+                    <button type="submit" class="btn btn-primary">Kaydet</button>
+                </div>
+            </form>
+        </Modal>
     </div>
     <!-- END WRAPPER -->
 </template>
 
 <script setup>
+import Input from '../components/Input.vue';
+import Modal from '../components/Modal.vue';
+
 </script>

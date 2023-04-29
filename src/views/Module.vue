bf52dd1aaf9d1de6ae8024ad0a7ad0fb36eaea00
batuhanarslan <batuhan.ars@yahoo.com>
2023-04-29 16:27:26 +0300

    Modül içerik güncelleme alanları hazır.

--- src/views/Module.vue
+++ src/views/Module.vue
@@ -1,9 +1,45 @@
 <template>
     <div class="container-fluid">
-        <h3 class="page-title">Üye Giriş Kodu Gönderme</h3>
-        <h4 class="page-title"><span style="margin-right: 7px"
-                class="label label-default">https://yolhizmeti.com/api/v1/corporate/send-code</span><span
-                class="label label-primary">POST</span></h4>
+        <div style="display: flex; align-items:center; justify-content:space-between;margin-bottom: 30px;">
+            <div style="display: flex; align-items:center;">
+                <h3 class="page-title" style="margin: 0;" v-if="editTitle">
+                    Üye Giriş Kodu Gönderme
+                </h3>
+                <input type="text" class="form-control" value="Üye Giriş Kodu Gönderme" v-else>
+                <a href="#" @click="editTitle = false" v-if="editTitle">
+                    <i class="fas fa-pen text-primary" style="font-size: 16px; margin-left:15px;"></i>
+                </a>
+                <a href="#" @click="editTitle = true" v-else>
+                    <i class="fas fa-times text-danger" style="font-size: 16px; margin-left:15px;"></i>
+                </a>
+            </div>
+        </div>
+        <h4 class="page-title" style="display:flex; align-items:center;">
+            <div v-if="editEndpoint">
+                <span style="margin-right: 7px"
+                    class="label label-default">https://yolhizmeti.com/api/v1/corporate/send-code</span>
+                <span class="label label-primary">POST</span>
+            </div>
+            <div v-else class="row">
+                <div class="col-md-8" style="padding-right: 0;">
+                    <input type="text" class="form-control" value="https://yolhizmeti.com/api/v1/corporate/send-code">
+                </div>
+                <div class="col-md-4">
+                    <select class="form-control" value="POST">
+                        <option value="">GET</option>
+                        <option value="">POST</option>
+                        <option value="">PUT</option>
+                        <option value="">DELETE</option>
+                    </select>
+                </div>
+            </div>
+            <a href="#" style="font-size: 16px; margin-left: 15px;" v-if="editEndpoint" @click="editEndpoint = false">
+                <i class="fas fa-pen"></i>
+            </a>
+            <a href="#" style="font-size: 16px; margin-left: 15px;" v-else @click="editEndpoint = true">
+                <i class="fas fa-times text-danger"></i>
+            </a>
+        </h4>
         <div class="row">
             <div class="col-md-5">
                 <div class="panel">
@@ -59,5 +95,9 @@
 </template>
 
 <script setup>
+import { ref } from "vue";
 
+
+const editEndpoint = ref(true)
+const editTitle = ref(true)
 </script>

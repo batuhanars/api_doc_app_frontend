765dc78f475a1af1d1f2f18d8d5265847a0350d9
batuhanarslan <batuhan.ars@yahoo.com>
2023-05-06 12:22:23 +0300

    Input componenti revize edildi.

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,21 +1,14 @@
 <template>
   <div class="form-group" v-if="type == 'file'">
     <label :for="forLabel" class="form-label">{{ label }}</label>
-    <input
-      type="file"
-      @change="$emit('update:modelValue', $event.target.files[0])"
-      class="form-control"
-    />
+    <small class="text-danger" style="margin-left: 5px">{{ error }}</small>
+    <input type="file" @change="$emit('update:modelValue', $event.target.files[0])" class="form-control" />
   </div>
   <div class="form-group" v-else>
     <label :for="forLabel" class="form-label">{{ label }}</label>
-    <input
-      :type="type"
-      :value="modelValue"
-      @input="$emit('update:modelValue', $event.target.value)"
-      :placeholder="label"
-      class="form-control"
-    />
+    <small class="text-danger" style="margin-left: 5px">{{ error }}</small>
+    <input :type="type" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" :placeholder="label"
+      class="form-control" />
   </div>
 </template>
 
@@ -25,5 +18,6 @@ const props = defineProps({
   modelValue: [String, Object],
   forLabel: String,
   type: String,
+  error: String,
 });
 </script>
